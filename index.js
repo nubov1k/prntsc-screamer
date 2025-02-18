63756d8b7a86d86f8dcfcd4f25f2b122362fd03f
Nikita <50026919+nubovik01@users.noreply.github.com>
2025-02-18 19:42:23 +0300

    changes

--- index.js
+++ index.js
@@ -5,27 +5,42 @@ const acceptButton = document.getElementById("accept-button");
 
 let hasClicked;
 
-window.onbeforeunload = function( ) {
-    if(hasClicked) return true;
+window.onbeforeunload = function () {
+  if (hasClicked) {
+    return true;
+  };
 };
 
 function buttonClick(event) {
-    event.preventDefault();
-    if(!hasClicked) hasClicked = true;
-    overlay.hidden = true;
-    video.play();
-    videoClick();
-}
+  event.preventDefault();
+
+  if (!hasClicked) {
+    hasClicked = true;
+  };
+
+  overlay.hidden = true;
+
+  video.play();
+  videoClick();
+};
 
 function videoClick(event) {
-    if(event) event.preventDefault();
-    // if not fullscreen
-    const { documentElement } = document;
-    if(documentElement.requestFullscreen) documentElement.requestFullscreen();
-    else if(documentElement.mozRequestFullScreen) documentElement.mozRequestFullScreen();
-    else if(documentElement.webkitRequestFullscreen) documentElement.webkitRequestFullscreen();
-    else if(documentElement.msRequestFullscreen) documentElement.msRequestFullscreen();
-}
+  if (event) {
+    event.preventDefault();
+  };
+
+  const { documentElement } = document;
+
+  if (documentElement.requestFullscreen) {
+    documentElement.requestFullscreen();
+  } else if (documentElement.mozRequestFullScreen) {
+    documentElement.mozRequestFullScreen();
+  } else if (documentElement.webkitRequestFullscreen) {
+    documentElement.webkitRequestFullscreen();
+  } else if (documentElement.msRequestFullscreen) {
+    documentElement.msRequestFullscreen();
+  };
+};
 
 acceptButton.addEventListener("click", buttonClick);
 declineButton.addEventListener("click", buttonClick);
